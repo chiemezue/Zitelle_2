9e629b2cd76c8ad9cb2cf1ad0699d4277652c5c7
chiemezue <113723263+chiemezue@users.noreply.github.com>
2026-05-23 13:18:08 +0100

    added the about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
+import PageHero from "../Components/PageHero";
+import AboutIntro from "../Components/AboutIntro";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <PageHero
+        title="About"
+        highlight="Us"
+        image="https://images.unsplash.com/photo-1565514020179-026b92b84bb6?q=80&w=1800&auto=format&fit=crop"
+        breadcrumb={
+          <>
+            <span className="page-hero__crumb">Home /</span>
+
+            <span className="page-hero__crumb--active">About</span>
+          </>
+        }
+      />
+      <AboutIntro />
+    </>
+  );
 };
 
 export default About;
